389b431aa032ada6fa7329f03737b895e33b0f54
kh931623 <kh931623@hotmail.com>
2019-05-12 01:30:46 +0800

    heroku setting

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 // console.log(process.env.codeDebug);
 
 // connect to mongodb
-mongoose.connect(config.mongo, {
+mongoose.connect('mongodb+srv://masterc:80122399@cluster0-o3s5l.azure.mongodb.net/express_ec?retryWrites=true', {
   useNewUrlParser: true
 });
 mongoose.connection.on('error', () => {
