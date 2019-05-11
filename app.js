afdff455691349cd0c43d7477c0aeaefe9b8d480
kh931623 <kh931623@hotmail.com>
2019-05-12 01:35:31 +0800

    fix heroku setting

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 // self-defined
-const config = require('./configs/config.js');
+// const config = require('./configs/config.js');
 
 // routers
 const indexRouter = require('./routes/index');
